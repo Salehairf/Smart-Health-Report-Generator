8415e84a3270a7d10e7224ada35530bf594e754f
Salehairf <swalehairfan321@gmail.com>
2025-10-18 14:18:52 +0530

    Add requirements.txt and app.py

---
+++ app.py
@@ -0,0 +1,120 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+
+
+# Load API key
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+#Extract text from PDFs 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+#Split text into chunks 
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+#Create and save FAISS vector store
+def get_vector_store(text_chunks):
+    # Using HuggingFace MiniLM for embeddings
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+#Conversational QA chain
+def get_conversational_chain():
+    
+
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context.
+    If the answer is not in the context, just say "Answer is not available in the context".
+    Do not make up an answer.
+
+    Context:\n {context}\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(
+    model="models/gemini-2.5-flash-lite", 
+    temperature=0.3
+)
+
+
+
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+# Handle user input 
+def user_input(user_question):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+
+    st.write("Reply:", response["output_text"])
+
+
+#Streamlit App
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Medical-Report-Summarizer")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader(
+            "Upload your PDF Files and Click on Submit & Process",
+            accept_multiple_files=True
+        )
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Vector DB Ready")
+
+
+if __name__ == "__main__":
+    main()
